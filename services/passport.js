fe976bfdc0c3fc93e3fe026e7e174a4844b3f184
Dmitry Bondarenko <d.bondrnko@gmail.com>
2017-09-21 19:05:12 +0300

    Added MongoDB

---
+++ services/passport.js
@@ -0,0 +1,40 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('user');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => done(null, user))
+        .catch(err => done(err));
+});
+
+
+passport.use(new GoogleStrategy({
+    clientID: Keys.googleClientID,
+    clientSecret: Keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id })
+        .then(existUser => {
+            if (existUser) {
+                done(null, existUser);
+            } else {
+                new User({ googleId: profile.id })
+                    .save()
+                    .then(user => done(null, user));
+            }
+        }).catch(err => {
+        done(err);
+    });
+
+
+}));
+
+
